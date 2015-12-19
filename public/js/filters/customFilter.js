6f14ad1477d21f38f5a857a1c7c088867ccd9348
variu <variu@naver.com>
2015-12-19 11:44:46 +0900

    15/12/19 포인트 상점 관련 ng-class 및 페이징 추가

--- public/js/filters/customFilter.js
+++ public/js/filters/customFilter.js
@@ -1,4 +1,4 @@
-
+// 파라미터 중 data 는 ng-repeat 관련해서 데이터를 자동으로 받고, 그 뒤로는 파라미터이다.
 
 angular.module('customFilter',[])
 .filter('unique', function(){
@@ -37,6 +37,38 @@ angular.module('customFilter',[])
 					keys[val] = true;
 					results.push(val);
 				}
+			}
+			return results;
+		}
+		else{
+			return data;
+		}
+	}
+})
+.filter('range', function($filter){
+	return function(data, page, size){	
+		if(angular.isArray(data) && angular.isNumber(page) && angular.isNumber(size)){
+			var startIndex = (page - 1) * size;
+			if(data.length < startIndex){
+				return [];
+			}else{
+				return $filter('limitTo')(data.splice(startIndex), size);	// 새로운 배열을 만들어준다.
+			}
+		}
+		else{
+			return data;
+		}
+
+
+
+	}
+})
+.filter('pageCount', function(){
+	return function(data, size){	
+		if(angular.isArray(data)){
+			var results=[];
+			for(var i=0; i<Math.ceil(data.length / size); i++){
+				results.push(i);	
 			}
 			return results;
 		}
