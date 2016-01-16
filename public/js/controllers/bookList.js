3b6b0f902e82d96dfd8133888380102855af04bf
variu <variu@naver.com>
2016-01-16 09:45:31 +0900

    서점 페이징 추가 및 클릭 시 class 이벤트 추가

--- public/js/controllers/bookList.js
+++ public/js/controllers/bookList.js
@@ -1,11 +1,14 @@
 angular.module('book')
-.controller('bookListCtrl', function($scope, $filter){
+.constant('bookListActiveClass', 'btn-success')		
+.constant('bookListPageCount', 2)
+.controller('bookListCtrl', function($scope, $filter, bookListActiveClass, bookListPageCount) {
 	var selectedGrade = null;
 	$scope.scopeOrderBy = false;	// true : asc , false : desc
 	$scope.scopeStar = false;		// 별로 만들건지 말건지
 
 	$scope.selectGrade = function(newGrade){
-		selectedGrade = newGrade==undefined?null:parseInt(newGrade);		
+		selectedGrade = newGrade==undefined?null:parseInt(newGrade);
+		$scope.selectedPage = 1;		
 	}
 
 	$scope.gradeFilterFn = function(book){
@@ -38,5 +41,35 @@ angular.module('book')
 		}
 		
 	}
+
+	// 페이징 관련
+	$scope.selectedPage = 1;
+	$scope.pageSize = bookListPageCount;
+
+	$scope.selectPage = function(newPage){
+		$scope.selectedPage = newPage;
+	}
+
+	$scope.getPageClass = function(page){
+		return ($scope.selectedPage == page)?bookListActiveClass : '';
+	}
+
+	// 등급에 따른 클래스 이벤트
+	$scope.getPageClass = function(page){
+		return $scope.selectedPage == page? bookListActiveClass : '';
+	};
+
+	// 클릭하면 내용 보여주기
+	$scope.show = function(book){
+		var msg = '';
+
+             	for(prop in book){
+	                 	if(prop != '$$hashKey'){
+	                     		msg += prop + ' : ' + book[prop] + '\n';
+	                 	}
+	             }
+
+	             alert(msg);
+         	}
 
 });
